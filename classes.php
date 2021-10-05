651f172f040d410abe14b35a442572acaffb53c5
linuxid <olexa.lexa@gmail.com>
2021-10-05 15:26:16 +0300

    Positive picture adding

--- classes.php
+++ classes.php
@@ -5,16 +5,19 @@ class Question
     public $a=array();
     public $corr=0;
     private static $keys=array();
+    public $pic="";
     function __construct($c)
     {
         $this->condition=$c[0];
         for ($i=1;$i<6;$i++){
             $this->a[]=$c[$i];
         }
+        if ($c[6]!="0") $this->pic=substr($c[6],1,-1);
     }
     function Show($num)
     {
         echo ($num+1).') '.$this->condition.'<br>';
+        if ($this->pic!="0") {echo '<img src="'.$this->pic.'" width=320px alt="'.$this->pic.'"><br>';}
         echo '<select class="n" onclick="className=selectedIndex?\'y\':\'n\'" name=task'.($num+1).'>';
         echo '<option> ---------------------------------------------------------------</option>';
         for ($i=0;$i<5;$i++) {
